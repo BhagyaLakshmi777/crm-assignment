80e35ae1be0c8539276e6bf6de93fde20d5f70f9
Bhagya Lakshmi <pbhagya2001@gmail.com>
2025-11-04 16:29:42 +0530

    Wrote all APIs

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
 
 const {open} = require("sqlite");
-const sqlite3 = require("sqlite3");
+const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 app.use(express.json());
-
-
 
 require("dotenv").config();  
 
@@ -24,6 +22,7 @@ const secretToken = process.env.SECRETTOKEN || "MY_SECRET_TOKEN"
 
 let db = null;
 
+
 const initializeDbAndServer = async () => { 
   try{
     db = await open({
@@ -32,6 +31,7 @@ const initializeDbAndServer = async () => {
     })
     app.listen(port, hostname, ()=>{
       console.log(`Server is running: http://${hostname}:${port}/`)
+
     })
   }catch(e){
     console.log(`DB error: ${e.message}`)
@@ -39,13 +39,13 @@ const initializeDbAndServer = async () => {
   }
 
 }
-initializeDbAndServer()  
+initializeDbAndServer()   
 
 //api 1 
 
 app.post("/api/employees/register", async (request,response) =>{
   const {email, password, name} = request.body;
-  const hashPassword = await bcrypt.hash(password,10)
+  const hashPassword = await bcrypt.hash(password, 10)
   const selectUserQuery = `
                             SELECT * FROM Employee
                             WHERE email = '${email}';
@@ -53,7 +53,7 @@ app.post("/api/employees/register", async (request,response) =>{
   const dbUser = await db.get(selectUserQuery)
   if (dbUser === undefined){
     const createUser = ` 
-        INSERT INTO Employee (name, email, password) VALUES ('${email}', '${hashPassword}', '${name}');
+        INSERT INTO Employee (name, email, password) VALUES ('${name}', '${email}', '${hashPassword}');
      `;
      await db.run(createUser)
      response.status(200)
@@ -68,7 +68,7 @@ app.post("/api/employees/register", async (request,response) =>{
 
 //api 2 
 
-app.post("/api/employees/login", async (request, response)=>{
+app.post("/api/employees/login", async (request, response) =>{
   const {email, password} = request.body;
   const selectUserQuery = `
                             SELECT * FROM Employee
@@ -84,6 +84,7 @@ app.post("/api/employees/login", async (request, response)=>{
       const isPasswordMatch = await bcrypt.compare(password, dbUser.password);
       if (isPasswordMatch){
         const payload = {id: dbUser.id}
+        console.log(payload)
         const jwtToken = jwt.sign(payload, '${secretToken}')
         response.send({jwtToken})
       }
@@ -94,7 +95,6 @@ app.post("/api/employees/login", async (request, response)=>{
     }
 
 })
-
 //api 3
 const authenticateToken = (request, response, next) => {
   let jwtToken;
@@ -104,12 +104,12 @@ const authenticateToken = (request, response, next) => {
   }
   if (jwtToken === undefined) {
     response.status(401);
-    response.send("Invalid JWT Token");
+    response.send("Unauthorized");
   } else {
     jwt.verify(jwtToken, '${secretToken}', async (error, payload) => {
       if (error) {
         response.status(401);
-        response.send("Invalid JWT Token");
+        response.send("Unauthorized");
       } else {
         request.id = payload.id
         next();
@@ -117,25 +117,22 @@ const authenticateToken = (request, response, next) => {
     });
   }
 };
-
 //api 4 
-app.post("/api/enquiries/public", async (request,response)=>{
-  const { name, email, courseInterest} = request.body;
+app.post("/api/enquiries/public", async (request, response)=> {
+ const {id, name, email, courseInterest} = request.body;
  const insertQuery = `
-      INSERT INTO Enquiry (name, email, courseInterest, claimed, counselorId)
-      VALUES ('${name}, '${email}', '${courseInterest}', FALSE, NULL)
+                     INSERT INTO Enquiry(id, name, email, courseInterest, claimed, counselorId) VALUES(${id}, '${name}', '${email}', '${courseInterest}', 0, NULL)
     `;
-   await db.run(insertQuery)
-   response.status(201);
-   response.send("Created")
-  
+ await db.run(insertQuery)
+ response.status(201);
+ response.send("Created")
 })
 
 //api 5 
 app.get("/api/enquiries/public", authenticateToken, async (request,response)=>{
   const selectEnquiry = `
                            SELECT * FROM Enquiry 
-                           WHERE claimed = FALSE AND counselorId IS NULL;
+                           WHERE claimed = 0 AND counselorId IS NULL;
   `;
   const dbEnquiries = await db.all(selectEnquiry);
   response.status(200)
@@ -157,27 +154,28 @@ app.get("/api/enquiries/private", authenticateToken, async (request,response)=>{
 
 //api 7 
 app.patch("/api/enquiries/:id/claim", authenticateToken, async (request, response)=>{
-  const {id} = request.params ;
+  const {id} = request.params;
+   console.log(request.id)
+  
     const selectEnquiry = `
                            SELECT * FROM Enquiry 
-                           WHERE counselorId = ${id};
+                           WHERE id = ${id};
   `;
   const dbResponse = await db.get(selectEnquiry);
-  if (dbResponse.claimed){
+  console.log(dbResponse)
+ 
+   if (dbResponse.claimed === 1){
     response.status(409);
     response.send("Conflict")
   }
   else{
-    const updateQuery =` UPDATE TABLE Enquiry 
-                        SET claimed = TRUE,
-                            counselorId = request.id
-                        WHERE counselorId = ${id};
-                    `;
+    const updateQuery =` UPDATE Enquiry SET claimed = 1, counselorId = ${request.id}
+                          WHERE id = ${id};
+    `;
     await db.run(updateQuery)
     const selectEnquiry = `SELECT * FROM Enquiry 
-                           WHERE counselorId = ${request.id};
-                          
-    `
+                           WHERE id = ${id};
+                          `;
     const dbResponse = await db.get(selectEnquiry);
     response.send(dbResponse)
 
